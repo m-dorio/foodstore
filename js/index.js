2177e0c2f71299462cef4853627d3ea195559f50
Michael Angelo Dorio <michaelangelo.dorio@gmail.com>
2023-07-31 00:02:29 +0800

    updated teh js for filter

--- js/index.js
+++ js/index.js
@@ -82,16 +82,16 @@ window.addEventListener("scroll", function() {
 //   });
 
 
-  const f = document.getElementById('form');
-  const q = document.getElementById('query');
-  const google = 'https://www.google.com/search?q=site%3A+';
-  const site = 'https://m-dorio.github.io/foodstore/';
+  // const f = document.getElementById('form');
+  // const q = document.getElementById('query');
+  // const google = 'https://www.google.com/search?q=site%3A+';
+  // const site = 'https://m-dorio.github.io/foodstore/';
 
-  function submitted(event) {
-    event.preventDefault();
-    const url = google + site + '+' + q.value;
-    const win = window.open(url, '_blank');
-    win.focus();
-  }
+  // function submitted(event) {
+  //   event.preventDefault();
+  //   const url = google + site + '+' + q.value;
+  //   const win = window.open(url, '_blank');
+  //   win.focus();
+  // }
 
-  f.addEventListener('submit', submitted);
+  // f.addEventListener('submit', submitted);
