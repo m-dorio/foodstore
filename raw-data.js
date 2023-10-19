c864708a91b16e4d61b6bcadec8c64b6ddfa70a6
Michael Angelo Dorio <134288177+m-dorio@users.noreply.github.com>
2023-10-19 21:08:26 +0800

    Create raw-data.js

---
+++ raw-data.js
@@ -0,0 +1,97 @@
+{
+  "_id": {
+    "$oid": "6506b90fb40dbaa150cb90fb"
+  },
+  "name": "Ramen (Japan)",
+  "description": "Ramen is a Japanese noodle soup dish with Chinese origins. It typically consists of wheat noodles served in a flavorful broth, topped with various ingredients like sliced pork, soft-boiled egg, seaweed, and green onions.",
+  "price": 240,
+  "isActive": true,
+  "createdOn": {
+    "$date": "2023-09-17T07:00:04.152Z"
+  },
+  "productImg": "https://m-dorio.github.io/foodstore/images/img-1.jpg",
+  "quantity": 18,
+  "ratings": [
+    {
+      "userId": "6501dcfffdc8dbe5484f8d51",
+      "userImg": "https://hips.hearstapps.com/hmg-prod/images/henry-cavill-geralt-of-rivia-the-witcher-1639401519.jpg?crop=0.692xw:1.00xh;0.155xw,0&resize=1200:*",
+      "userName": "El Em",
+      "comment": "Homemade ramen night! 🍜 Added some soft-boiled eggs and extra nori for that authentic touch. Loving every slurp!",
+      "date": "1695000752381",
+      "isActive": "true",
+      "_id": {
+        "$oid": "6507a8b0ec4f14208045b618"
+      }
+    },
+    {
+      "userId": "64ff821b58dcbadc69b553c6",
+      "userImg": "https://hips.hearstapps.com/hmg-prod/images/gettyimages-3066352.jpg?resize=1200:*",
+      "userName": "Adas Lovelace",
+      "comment": "I'm on a mission to find the spiciest ramen in town! Any recommendations for places that serve extra fiery bowls?",
+      "date": "1695001117933",
+      "isActive": "true",
+      "_id": {
+        "$oid": "6507aa1dd4727f22327da749"
+      }
+    },
+    {
+      "userId": "64ff821b58dcbadc69b553c6",
+      "userImg": "https://hips.hearstapps.com/hmg-prod/images/gettyimages-3066352.jpg?resize=1200:*",
+      "userName": "Adas Lovelace",
+      "comment": "\"Ramen weather is officially here! 🌧️ Nothing beats cozying up with a hot bowl of shoyu ramen on a chilly day.",
+      "date": "1695001324518",
+      "isActive": "true",
+      "_id": {
+        "$oid": "6507aaec027b91f7ca3f98af"
+      }
+    },
+    {
+      "userId": "650974cbdbc1741b09ae33f5",
+      "userImg": "",
+      "userName": "Peter Parker",
+      "comment": "The ramen is spider-man approved.",
+      "rate": 10,
+      "date": "1695118678207",
+      "isActive": "true",
+      "_id": {
+        "$oid": "65097556dbc1741b09ae49db"
+      }
+    },
+    {
+      "userName": "El Em",
+      "comment": "test",
+      "rate": 4,
+      "date": "1695149792140",
+      "isActive": "true",
+      "_id": {
+        "$oid": "6509eee0e19fb1e8fda0ffe6"
+      }
+    },
+    {
+      "userId": "6501dcfffdc8dbe5484f8d51",
+      "userImg": "https://cdn.dribbble.com/users/9685/screenshots/997495/avatarzzz.gif",
+      "userName": "El Em",
+      "comment": "Can Ramen \"Sort\" this out? This comment should be placed on top of the others.",
+      "rate": 4,
+      "date": "1695150730603",
+      "isActive": "true",
+      "_id": {
+        "$oid": "6509f28ae19fb1e8fda41ac3"
+      }
+    },
+    {
+      "userId": "64ff821b58dcbadc69b553c6",
+      "userImg": "https://hips.hearstapps.com/hmg-prod/images/gettyimages-3066352.jpg?resize=1200:*",
+      "userName": "Ada Lovelace",
+      "comment": "Test",
+      "rate": 5,
+      "date": "1695313238578",
+      "isActive": "true",
+      "_id": {
+        "$oid": "650c6d564a4ab91e36ff7e27"
+      }
+    }
+  ],
+  "__v": 11,
+  "averageRating": 3
+}
